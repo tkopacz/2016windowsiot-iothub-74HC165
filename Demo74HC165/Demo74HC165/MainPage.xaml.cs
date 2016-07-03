f06e90ba8acc9ea1fe05ce75c12cd2245fc3ab8f
Tomasz Kopacz <tkopacz@tomaszkopacz.com>
2016-07-03 20:10:35 +0200

    work

---
+++ Demo74HC165/Demo74HC165/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.IoT.Lightning.Providers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Devices;
+using Windows.Devices.Gpio;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Demo74HC165
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            setup();
+        }
+
+        private async Task setup()
+        {
+            if (LightningProvider.IsLightningEnabled)
+            {
+                // Set Lightning as the default provider
+                LowLevelDevicesController.DefaultProvider = LightningProvider.GetAggregateProvider();
+            }
+            else
+            {
+                throw new Exception("NoLighting!");
+            }
+
+            var gpioController = GpioController.GetDefault(); /* Get the default GPIO controller on the system */
+            var p = gpioController.OpenPin(5);
+            p.SetDriveMode(GpioPinDriveMode.Output);
+            p.Write(GpioPinValue.High);
+            p.Write(GpioPinValue.Low);
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # 2016windowsiot-iothub-74HC165
 Windows Iot Shift register 74HC165
+No SPI
+Used: Microsoft.IoT.Lightning
